37be4b10bffd9a46e69172799d5396fa789d4c69
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2026-02-09 19:44:56 -0300

    Config deploy gunicorn supervisor sgrh

--- app/deploy/gunicorn.sh
+++ app/deploy/gunicorn.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-NAME="factora"
-DIRECTORY=$(cd `dirname $0` && pwd)
-DJANGODIR=`dirname $DIRECTORY`
-SOCKFILE=/tmp/gunicorn.sock
-LOGDIR=${DJANGODIR}/logs/gunicorn.log
-USER=jair
-GROUP=jair
-NUM_WORKERS=5
-DJANGO_SETTINGS_MODULE=config.production
+NAME="sgrh"
+DJANGODIR=/home/tic/sgrh
+SOCKFILE=/home/tic/sgrh/run/gunicorn.sock
+USER=tic
+GROUP=tic
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=config.settings
 DJANGO_WSGI_MODULE=config.wsgi
 
-rm -frv $SOCKFILE
+echo "Iniciando aplicación $NAME"
 
-echo "Iniciando la aplicación $NAME con el usuario `whoami`"
-
+# Ir al directorio del proyecto
 cd $DJANGODIR
 
-exec /home/jair/factora/env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+# Activar el entorno virtual usando tu ruta específica
+source /home/tic/sgrh/.env/bin/activate
+
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Crear el directorio para el socket si no existe
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Ejecutar Gunicorn
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
   --bind=unix:$SOCKFILE \
   --log-level=debug \
-  --log-file=$LOGDIR
+  --log-file=-
